868766e64ab8566335a954250e09fd7f9eb16d17
Enijeshiku <eenijeshiku@gmail.com>
2025-09-13 00:38:35 +0100

    chore: Added 03_fs for exercise solution

---
+++ C-LABS/01Rock_Paper_Scissors.c
@@ -0,0 +1,12 @@
+/*In this Rock Paper Scissor project, 
+users are asked to make a choice between these three options. 
+The computer will also randomly choose one of the three shapes. 
+Once both the user and the computer have made their choices, 
+the result will be displayed, showing who won the round along with the choices of both players.*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+int main () {
+   int humanChoice, computer;
+
+}
