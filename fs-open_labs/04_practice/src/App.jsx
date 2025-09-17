860afa69715c08458c31d7f543e064c37da6d235
Enijeshiku <eenijeshiku@gmail.com>
2025-09-17 21:46:53 +0100

    Attempting exercise 1.4

---
+++ fs-open_labs/04_practice/src/App.jsx
@@ -0,0 +1,27 @@
+// 1.4: Course Information step 4
+// Place the objects into an array. 
+// Modify the variable definitions of App into the following form and modify the other parts of the application accordingly
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
+
+  return (
+    <div>
+      ...
+    </div>
+  )
+}
