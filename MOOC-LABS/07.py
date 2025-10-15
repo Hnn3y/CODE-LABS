333fe33561385c110f14e2de5a1412d98aea2771
Enijeshiku <eenijeshiku@gmail.com>
2025-10-15 15:23:22 +0100

    .

--- MOOC-LABS/07.py
+++ MOOC-LABS/07.py
@@ -1,7 +1,6 @@
 num = int(input("Enter a number: "))
 
 if num % 2==0:
-   if num % 3 == 0:
-      print(f"The number {num} is an even number")
-   else:
-      print(f"The number {num} is an odd number")
+   print(f"The number {num} is an even number")
+else:
+   print(f"The number {num} is an odd number")
