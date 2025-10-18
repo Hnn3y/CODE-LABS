db68e0813c3ea1222d1497c3abba3c601f9fdc72
Enijeshiku <eenijeshiku@gmail.com>
2025-10-18 21:49:50 +0100

    .

---
+++ MOOC-LABS/08.py
@@ -0,0 +1,7 @@
+#Write a program that asks the user for a temperature in Celsius and converts it to Fahrenheit using the formula: F = (C * 9/5) + 32. 
+# Print the result.
+
+C = int(input("Enter temperature in Celsius: "))
+
+F = (C * 9/5) + 32
+print(f"{C} degrees Celsius is 77.0 degrees Fahrenheit")
