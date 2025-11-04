951e711397948b9fe784633fe0869e198794bf98
Enijeshiku <eenijeshiku@gmail.com>
2025-11-04 14:31:52 +0100

    .

--- fs-open_labs/07_lab/src/App.jsx
+++ fs-open_labs/07_lab/src/App.jsx
@@ -1,4 +1,14 @@
 const App = () => {
+  const students = [
+    { name: 'John', grade: 85 },
+    { name: 'Jane', grade: 92 },
+    { name: 'Bob', grade: 78 }
+  ]
+
+  console.log(students[1].grade)
+  students.map(student => console.log(student.name))
+  students.map(student => console.log(student.grade+5))
+
 
   return(
     <>
@@ -7,3 +17,9 @@ const App = () => {
 }
 
 export default App
+
+
+
+// TODO: Console log Jane's grade
+// TODO: Use .map() to create an array of just the names
+// TODO: Use .map() to create an array of grades increased by 5
